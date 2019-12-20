d4537df4adc914ad2182a5d54054e74fed8647e2
Student <hubert.welp@thga.de>
2019-12-20 11:45:32 +0100

    python dummy mit Parameterübergabe und Ausgabe in txt Datei

--- SP3Objekterkenner/SP3Objekterkenner.py
+++ SP3Objekterkenner/SP3Objekterkenner.py
@@ -1,1 +1,13 @@
-print("Aufruf erfolgreich")
+import sys
+
+print (sys.argv[1])
+print (sys.argv[2])
+
+#file = open("helloworld.txt")
+file = open("helloworld.txt","w")
+
+file.write(sys.argv[1])
+file.write("\n")
+file.write(sys.argv[2])
+
+file.close
