4609ac0fe2fe4004735bf4eaa008926b04e4f886
Rami Alkhooli <rami.alkhooli@stud.thga.de>
2020-01-08 14:44:04 +0100

    Bildanalysator erkennt 4 Anfragen und kann abbrechen

--- SP3Bildanalysator/main.cpp
+++ SP3Bildanalysator/main.cpp
@@ -3,23 +3,25 @@
 
 int main(int argc, char *argv[])
 {
-    Kamera c("/home/Student/git/SweetPicker3/SP3Bildanalysator");
+    int wahl;
 
-    printf("\nDrücken Sie f für ein Bild / c zum Schließen\n");
+    // Erstellen eines Objekts
+    Kamera c("/home/Student/git/SweetPicker3/SP3Bildanalysator");
 
     while(1)
     {
-        char ant;
-        scanf("%c",&ant);
+        // Empfangen eines Zeichens (später hört es den UDP-Node ab und empfängt ein Zeichen)
+        printf("\nGeben Sie 1,2,3 oder 4 für ein Bild / 0 zum Schließen\n");
+        scanf("%i",&wahl);
 
-        if(ant == 'f')
+        // Auswertung der Eingabe
+        if(wahl == 1 || wahl == 2 || wahl == 3 || wahl == 4)
         {
             c.nehmeAuf("/SP3Bilderkennung/aktuelleSzene.jpg");
         }
 
-        else if(ant == 'c')
+        else if(wahl == 0)
         break;
-
     }
     return 0;
 }
