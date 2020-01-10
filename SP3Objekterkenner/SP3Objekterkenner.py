77f13ddaf0a0def0d9a67a16f14c777267c76842
--global <marieolbrich@web.de>
2020-01-10 12:20:36 +0100

    Textausgabe in übergebenen Pfad

--- SP3Objekterkenner/SP3Objekterkenner.py
+++ SP3Objekterkenner/SP3Objekterkenner.py
@@ -1,11 +1,16 @@
 import sys
+import os
 
 print (sys.argv[1])
 print (sys.argv[2])
 
-file = open("/home/Student/helloworld.txt","w")
+filepath = os.path.abspath(sys.argv[2])
+path = os.path.join(filepath, "helloworld.txt")
+
+print (path)
+file = open(path,"w")
 
 file.write(sys.argv[1])
-file.write("\n")
-file.write(sys.argv[2])
+#file.write("\n")
+#file.write(sys.argv[2])
 file.close
