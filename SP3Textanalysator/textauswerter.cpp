2ea9d02360ce653ca03fd12734e2af4963c7b1f0
rami-alkhooli <rami.al.kholi@gmail.com>
2020-04-04 21:47:35 +0200

    Schleife implementiert und erste Schritte in Funktionen

--- SP3Textanalysator/textauswerter.cpp
+++ SP3Textanalysator/textauswerter.cpp
@@ -1,8 +1,94 @@
 #include "textauswerter.h"
 
 Textauswerter::Textauswerter(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent), anzGlsnZeilen(0)
 {
+
+}
+
+bool Textauswerter::werteAus()
+{
+    QFile text (PFAD);
+
+    if(!text.open(QIODevice::ReadOnly))
+    {
+        QMessageBox::warning(this,"Datei öffnen","Datei konnte nicht geöffnet werden !");
+    }
+
+    QTextStream datenstrom;
+    QString string;
+
+    while(datenstrom.readLineInto(&string,MAXLESEN))
+    {
+        anzGlsnZeilen++;
+
+        if (string.contains(DCLASS,Qt::CaseInsensitive))
+        {
+            QMessageBox::warning(this,"Auswertung","Klassen werden ausgewertet");
+            detection_classes();
+        }
+        else if (string.contains(DSCORE,Qt::CaseInsensitive))
+        {
+            QMessageBox::warning(this,"Auswertung","Erkennungsraten werden ausgewertet");
+            detection_scores();
+        }
+        else if (string.contains(DBOX,Qt::CaseInsensitive))
+        {
+            QMessageBox::warning(this,"Auswertung","Boxen werden ausgewertet");
+            detection_boxes();
+        }
+    }
+}
+
+bool Textauswerter::detection_classes()
+{
+    // Die Text-Datei erneuert öffnen
+    QFile text (PFAD);
+    text.open(QIODevice::ReadOnly);
+    QTextStream datenstrom;
+    QString linie;
+
+    // Den Cursor sozusagen auf die aktuelle Zeile bringen
+    for (int n=0 ; n<anzGlsnZeilen ; n++) {datenstrom.readLineInto(&linie,MAXLESEN);}
+
+    // Hier die gewünschten Daten einlesen
+    // Erste Klammer entfernen
+    // Zahlen von jeder Zeile in Strings einlesen
+    // Letzte Klammer finden und Funktion beenden
+}
+
+bool Textauswerter::detection_scores()
+{
+    // Die Text-Datei erneuert öffnen
+    QFile text (PFAD);
+    text.open(QIODevice::ReadOnly);
+    QTextStream datenstrom;
+    QString linie;
+
+    // Den Cursor sozusagen auf die aktuelle Zeile bringen
+    for (int n=0 ; n<anzGlsnZeilen ; n++) {datenstrom.readLineInto(&linie,MAXLESEN);}
+
+    // Hier die gewünschten Daten einlesen
+    // Erste Klammer entfernen
+    // Zahlen von jeder Zeile in Strings einlesen
+    // Letzte Klammer finden und Funktion beenden
+}
+
+bool Textauswerter::detection_boxes()
+{
+    // Die Text-Datei erneuert öffnen
+    QFile text (PFAD);
+    text.open(QIODevice::ReadOnly);
+    QTextStream datenstrom;
+    QString linie;
+
+    // Den Cursor sozusagen auf die aktuelle Zeile bringen
+    for (int n=0 ; n<anzGlsnZeilen ; n++) {datenstrom.readLineInto(&linie,MAXLESEN);}
+
+    // Hier die gewünschten Daten einlesen
+    // Erste Klammer entfernen
+    // Zahlen von jeder Zeile in Strings einlesen
+    // Letzte Klammer finden und Funktion beenden
 }
 
 Textauswerter::~Textauswerter()
--- SP3Textanalysator/textauswerter.h
+++ SP3Textanalysator/textauswerter.h
@@ -1,7 +1,17 @@
 #ifndef TEXTAUSWERTER_H
 #define TEXTAUSWERTER_H
 
+#define PFAD "C:/SP3/gefundeneObjekteTest.txt"
+#define DCLASS "detection_classes"
+#define DSCORE "detection_scores"
+#define DBOX "detection_boxes"
+#define MAXLESEN 40
+
 #include <QWidget>
+#include <QFile>
+#include <QTextStream>
+#include <QString>
+#include <QMessageBox>
 
 class Textauswerter : public QWidget
 {
@@ -9,7 +19,14 @@ class Textauswerter : public QWidget
 
 public:
     Textauswerter(QWidget *parent = 0);
+    bool werteAus();
+    bool detection_classes(const QString zeile);
+    bool detection_scores(const QString zeile);
+    bool detection_boxes(const QString zeile);
     ~Textauswerter();
+
+private:
+    int anzGlsnZeilen;
 };
 
 #endif // TEXTAUSWERTER_H
