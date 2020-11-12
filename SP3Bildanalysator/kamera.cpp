b9494f555324f11c0b561fa3d250caf6b91f0123
C-Sahin <sahin-cosgun@hotmail.de>
2020-11-12 01:08:03 +0100

    Bugfix: Die Bildaufnahme des SP3Bildanalysators funktioniert nur einmalig nach dem Start. Danach wird immer das gleiche Bild verwendet.

--- SP3Bildanalysator/kamera.cpp
+++ SP3Bildanalysator/kamera.cpp
@@ -3,7 +3,10 @@
 Kamera::Kamera(const char* pfad)
 {
     pwd = pfad;
-    videostream = cv::VideoCapture (0);
+    videostream = cv::VideoCapture (0); // 0 = standardkamera
+    //videostream.set(CV_CAP_PROP_BUFFERSIZE, 1):
+
+
 }
 
 void Kamera::nehmeAuf(const char* pfad)
@@ -12,14 +15,24 @@ void Kamera::nehmeAuf(const char* pfad)
     cv::String ziel = cv::String (pwd) + cv::String(pfad);
 
     // Ein Bild mit der Kamera aufnehmen
-    videostream.read(bild);
+
+    /** videostream.open() first calls videostream.release() **/
+    if(!videostream.open(0))
+    {
+        printf("Kamera konnte nicht geoeffnet werden\n");
+    }
+    /** grabs, decodes and returns the next video frame **/
+    if(!videostream.read(bild))
+    {
+        printf("Bild konnte nicht erfasst werden\n");
+    }
 
     // Das Bild im angegebenen Pfad Speichern
     if( !cv::imwrite(ziel,bild) )
     {
-        printf("\nFehler bei der Speicherung !\n");
+        printf("Bild konnte nicht am Ziel gespeichert werden\n");
     }
 
     // Wartezeit, bis das Foto vollständig gespeichert ist
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+   // std::this_thread::sleep_for(std::chrono::milliseconds(100)); // ist das eig nötig? wenn imwrite "true" zurückgibt sollte das bild erfolgreich gespeichert sein
 }
