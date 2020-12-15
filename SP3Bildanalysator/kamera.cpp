9d677a94b3d9611aff3835390b249ec4665bc532
C-Sahin <sahin-cosgun@hotmail.de>
2020-12-15 17:05:56 +0100

    Rückgabeparameter von void zu int geändert. setzeKameraID() hinzugefügt

--- SP3Bildanalysator/kamera.cpp
+++ SP3Bildanalysator/kamera.cpp
@@ -3,33 +3,43 @@
 Kamera::Kamera(const char* pfad)
 {
     pwd = pfad;
-    videostream = cv::VideoCapture (0); // 0 = standardkamera
+    id = 0;
+    videostream = cv::VideoCapture (id); // 0 = standardkamera
     //videostream.set(CV_CAP_PROP_BUFFERSIZE, 1):
 
 
 }
-void Kamera::nehmeAuf(const char* pfad)
+/**
+ * @brief Kamera::nehmeAuf
+ * @param pfad
+ * @return < 0 wenn nicht erfolgreich. = 0 wenn erfolgreich
+ */
+int Kamera::nehmeAuf(const char* pfad)
 {
     // Das Zielpfad konstruieren
     cv::String ziel = cv::String (pwd) + cv::String(pfad);
 
     // Ein Bild mit der Kamera aufnehmen
     /** videostream.open() first calls videostream.release() **/
-    if(!videostream.open(0))
+    if(!videostream.open(id))
     {
         printf("Kamera konnte nicht geoeffnet werden\n");
+        return -1;
     }
     /** grabs, decodes and returns the next video frame **/
     if(!videostream.read(bild))
     {
         printf("Bild konnte nicht erfasst werden\n");
+        return -2;
     }
     // Das Bild im angegebenen Pfad Speichern
     if( !cv::imwrite(ziel,bild) )
     {
         printf("Bild konnte nicht am Ziel gespeichert werden\n");
+        return -3;
     }
     videostream.release();
+    return 0;
 }
 /**
  * @brief Kamera::nehmeAufTest überspringt die Kamera und übergibt direkt ein Bild aus den Trainingsdaten. nur für Testzwecke.
@@ -42,5 +52,26 @@ void Kamera::nehmeAufTest(const char *pfad)
     if( !cv::imwrite(ziel,bild) )
     {
         printf("Bild konnte nicht am Ziel gespeichert werden\n");
+    }
+}
+/**
+ * @brief Kamera::setzeKameraID Methode zum Kamera ändern. Wenn die angegebene ID zu keiner Kamera führt wird die ID verworfen und die vorherige ID weiter verwendet.
+ * @param idnew - neue Kamera ID
+ * @return < 0 wenn nicht erfolgreich. = 0 wenn erfolgreich.
+ */
+int Kamera::setzeKameraID(int idnew)
+{
+    if(!videostream.open(idnew))
+    {
+        std::cout << idnew << " ist keine gültige Kamera" << std::endl;
+        return -1;
+    }
+    else
+    {
+        /*Erzeuge neue Kamera mit neuer ID*/
+        videostream.~VideoCapture();
+        videostream = cv::VideoCapture (idnew);
+        id = idnew;
+        return 0;
     }
 }
--- SP3Bildanalysator/kamera.h
+++ SP3Bildanalysator/kamera.h
@@ -7,13 +7,15 @@ class Kamera
 {
 public:
     explicit Kamera(const char* pfad);
-    void nehmeAuf(const char* pfad);
+    int nehmeAuf(const char* pfad);
     void nehmeAufTest(const char* pfad);
+    int setzeKameraID(int idnew = 0);
 
 private:
     cv::VideoCapture videostream;
     cv::Mat bild;
     const char* pwd;
+    int id;
 };
 
 #endif // KAMERA_H
